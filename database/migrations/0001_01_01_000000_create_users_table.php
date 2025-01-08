2915d7b71a99c15439aade78282373ba49edff8e
leanderarya <aryaajisadda@gmail.com>
2025-01-08 10:43:03 +0700

    Role User

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('role')->default('operator');
             $table->string('password')->default('12345');
             $table->rememberToken();
             $table->timestamps();
