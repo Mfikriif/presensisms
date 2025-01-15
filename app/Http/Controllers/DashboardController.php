1b85a2bbe2dd3eab9bacb7e72eb4b9d901e8d13a
leanderarya <aryaajisadda@gmail.com>
2025-01-15 14:24:13 +0700

    UI Rekap Presensi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,14 +13,34 @@ class DashboardController extends Controller
     //
     public function index(){
         $hariini = date("Y-m-d");
+        $bulanini = date("m") * 1;
+        $tahunini = date("Y");
         $email = Auth::user()->email;
         $presensihariini = DB::table('presensi')
             ->where('email', $email)
             ->where('Tanggal_presensi', $hariini)
             ->first();
-    
+        $historibulanini = DB::table('presensi')
+            ->where('email',$email)
+            ->whereRaw('MONTH(Tanggal_presensi)="'.$bulanini.'"')
+            ->whereRaw('YEAR(Tanggal_presensi)="'.$tahunini.'"')
+            ->orderBy('Tanggal_presensi')
+            ->get();
+        $rekappresensi = DB::table('presensi')
+            ->selectRaw('COUNT(email) as hadir, SUM(IF(jam_in > "07:00",1,0)) as terlambat')
+            ->where('email',$email)
+            ->whereRaw('MONTH(Tanggal_presensi)="'.$bulanini.'"')
+            ->whereRaw('YEAR(Tanggal_presensi)="'.$tahunini.'"')
+            ->first();
+        $namabulan = ["","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+
         return Inertia::render('User/Dashboard', [
             'presensihariini' => $presensihariini,
+            'historibulanini' => $historibulanini,
+            'namabulan' => $namabulan,
+            'bulanini' => $bulanini,
+            'tahunini' => $tahunini,
+            'rekapPresensi' => $rekappresensi,
         ]);
     }
 }
